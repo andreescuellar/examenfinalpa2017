40470fa954ba5b5ecb5ee2eea453f047de696308
Andres Cuellar <31348623+andreescuellar@users.noreply.github.com>
2017-11-22 08:19:37 -0600

    commit 3

--- src/main/java/gt/edu/url/problema2/ImplArrayManipulator.java
+++ src/main/java/gt/edu/url/problema2/ImplArrayManipulator.java
@@ -12,7 +12,7 @@ public class ImplArrayManipulator<E> implements ArrayManipulator<E>  {
 		return 0;
 	
 	}
-
+	// se obtienen los datos a ser ordenados
 	public static <E> Set<Set<E>> findPowerSet(Set<E> set){
         Set<Set<E>> ret = new HashSet<Set<E>>();
         ret.add(set);
@@ -21,14 +21,14 @@ public class ImplArrayManipulator<E> implements ArrayManipulator<E>  {
         }
         Iterator<E> it = set.iterator();
         while(it.hasNext()){
-            Set<E> tmp = new HashSet<E>(set);   //create a copy of current set
-            tmp.remove(it.next());              //remove current element from copy set
-            ret.add(tmp);                       //add the remaining set to result
-            ret.addAll(findPowerSet(tmp));      //recursively find subsets of copy set
+            Set<E> tmp = new HashSet<E>(set);   //crea copia del set
+            tmp.remove(it.next());              //remueve el siguiente del set
+            ret.add(tmp);                       //agrega el temporario para el set
+            ret.addAll(findPowerSet(tmp));      //recusividad
         }
         return ret;
     }
-
+	//el main donde se le brindaran los datos para que hagan todas las posibles combinaciones
     public static void main(String[] args) {
         Set<Character> set = new HashSet<Character>();
         set.add('a');set.add('b');set.add('c');
@@ -36,7 +36,8 @@ public class ImplArrayManipulator<E> implements ArrayManipulator<E>  {
         System.out.println("\nsub sets");
         findPowerSet(set).stream().forEach(ImplArrayManipulator::printSet);
     }
-
+    
+    //impreme las combinaciones
     public static <E> void printSet(Set<E> set){
         StringBuilder sb = new StringBuilder(set.size()==0 ? "{}\n" :"{");
         Iterator<E> it = set.iterator();
